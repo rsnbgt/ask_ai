1852ff89fa110c551f1715bc6dce714ce0865ada
roshan <rsnbgt.com>
2025-08-17 05:29:53 +0530

    dashboard:1 Access to fetch at 'https://ask-ai-backend-89g3.onrender.com/api/chats' from origin 'http://localhost:5173' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource.Understand this error ask-ai-backend-89g3.onrender.com/api/chats:1  Failed to load resource: net::ERR_FAILEDwsQessage for your changes. Lines starting xqwqq

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,7 @@ dotenv.config();
 const port =process.env.PORT || 3000;
 const app=express();
 
-app.use(cors({
-    origin:process.env.CLIENT_URL,
-    credentials:true,
-}))
+app.use(cors({}))
 
 app.use(express.json())
 
